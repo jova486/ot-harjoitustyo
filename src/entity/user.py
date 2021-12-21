7df6ca1bba8adaf46b1cc4abf70ad03d67a6df8f
Jorma Valjakka <jorma.valjakka@helsinki.fi>
2021-12-21 16:07:30 +0200

    Viikko7_9

--- src/entity/user.py
+++ src/entity/user.py
@@ -1,24 +1,19 @@
 class User:
-    """Käyttäjää kuvaava luokka.
-
+    """Luokka, joka kuvaa yksittäistä käyttäjää.
     Attributes:
-        username: Käyttäjänimi
+        username: Käyttäjätunnus.
         password: Salasana
-        teacher: Kokonaislukuarvo: 1 = opettaja, 0 = oppilas
+        teacher: Kokonaislukuarvo 1 = opettaja 0 = oppilas
     """
 
     def __init__(self, username, password, teacher):
         """Luokan konstruktori.
-
         Args:
-            username: Käyttäjänimi
+            username: Käyttäjätunnus.
             password: Salasana
-            teacher: Kokonaislukuarvo: 1 = opettaja, 0 = oppilas
+            teacher: Kokonaislukuarvo 1 = opettaja 0 = oppilas
         """
 
         self.username = username
         self.password = password
         self.teacher = teacher
-
-
-    def __init__(self, username, password):
